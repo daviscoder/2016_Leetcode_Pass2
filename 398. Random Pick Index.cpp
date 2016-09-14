91faaa6680b3a28e7d055056ba269d65c198bce8
daviscoder <yunfeng.chn@gmail.com>
2016-09-14 14:25:15 -0700

    Update 398. Random Pick Index.cpp

--- 398. Random Pick Index.cpp
+++ 398. Random Pick Index.cpp
@@ -1,3 +1,8 @@
+/**
+ * 水塘抽样(reservoir sampling): When grabbing the k th number, we generate a random number p, if p < 1 / k, we keep the kth number, or, we reserve the previous one.
+ * 
+ **/
+
 class Solution {
 private:
     vector<int> nums;
