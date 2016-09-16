ea4a8d28d5475f052b91fdc9b1cf050941bf93a4
daviscoder <yunfeng.chn@gmail.com>
2016-09-16 00:54:48 -0700

    Create 020. Valid Parentheses.cpp

---
+++ 020. Valid Parentheses.cpp
@@ -0,0 +1,28 @@
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> myStack;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '{' || s[i] == '(' || s[i] == '[')
+                myStack.push (s[i]);
+            else if (s[i] == '}') {
+                if (myStack.empty() || myStack.top() != '{')
+                    return false;
+                myStack.pop();
+            }
+            else if (s[i] == ']') {
+                if (myStack.empty() || myStack.top() != '[')
+                    return false;
+                myStack.pop();
+            }
+            else {
+                if (myStack.empty() || myStack.top() != '(')
+                    return false;
+                myStack.pop();
+            }
+        }
+        return myStack.empty();
+    }
+};
