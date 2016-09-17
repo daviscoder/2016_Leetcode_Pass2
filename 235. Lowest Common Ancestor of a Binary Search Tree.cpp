318a1c1d94e35efaf3bcded8e3efe14c88b181b6
daviscoder <yunfeng.chn@gmail.com>
2016-09-17 01:13:35 -0700

    Update 235. Lowest Common Ancestor of a Binary Search Tree.cpp

--- 235. Lowest Common Ancestor of a Binary Search Tree.cpp
+++ 235. Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -1,11 +1,7 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+ * 如果当前节点为空或者与目标节点之一相同，则返回当前节点
+ * 递归寻找p和q在左右子树中的位置
+ * 如果p和q分别位于root的左右两侧，则root为它们的LCA，否则为左子树或者右子树
  */
 class Solution {
 public:
