f822949ef01e404debdae4fd24be2be82bcb08e6
daviscoder <yunfeng.chn@gmail.com>
2016-09-17 03:02:39 -0700

    Create 168. Excel Sheet Column Title.cpp

---
+++ 168. Excel Sheet Column Title.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string convertToTitle(int n) {
+        string res = "";
+        while (n) {
+            // 算remainder 的时候可以拿2, 26 举例... 对应B, Z. 不是 n % 26 + 'A' - 1
+            res = char((n - 1) % 26 + 'A') + res;
+            // 拿26举例即可...
+            n = (n - 1) / 26;
+        }
+        return res;
+    }
+};
