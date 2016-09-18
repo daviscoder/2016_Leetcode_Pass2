7cf665fcc2a0a46e07e418f6a1a698b6a3017d35
daviscoder <yunfeng.chn@gmail.com>
2016-09-18 00:56:36 -0700

    Create 173. Binary Search Tree Iterator.cpp

---
+++ 173. Binary Search Tree Iterator.cpp
@@ -0,0 +1,38 @@
+/**
+ * 保存所有左子树的父节点即可.
+ */
+class BSTIterator {
+public:
+    stack<TreeNode *> s;
+    BSTIterator(TreeNode *root) {
+        while (root) {
+            s.push(root);
+            root = root->left;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !s.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode * p = s.top();
+        s.pop();
+        if (p->right) {
+            p = p->right;
+            while (p) {
+                s.push(p);
+                p = p->left;
+            }
+        }
+        return p->val;
+    }
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
