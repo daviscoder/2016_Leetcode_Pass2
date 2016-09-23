b377aec597afe224d1799bc190ef92767874d652
daviscoder <yunfeng.chn@gmail.com>
2016-09-23 11:07:36 -0700

    Update 200. Number of Islands.cpp

--- 200. Number of Islands.cpp
+++ 200. Number of Islands.cpp
@@ -1,16 +1,20 @@
+/**
+ * O(m * n), O(1)
+ * 空grid, 没有island，全1 grid，部分为1 grid
+ */
+
 class Solution {
 public:
     void DFS (vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
-            return;
-        if (grid[i][j] == '0' || grid[i][j] == '2')
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != '1')
             return;
         grid[i][j] = '2';
         DFS (grid, i - 1, j);
-        DFS (grid, i + 1, j);
         DFS (grid, i, j - 1);
+        DFS (grid, i + 1, j);
         DFS (grid, i, j + 1);
     }
+
     int numIslands(vector<vector<char>>& grid) {
         int res = 0;
         for (int i = 0; i < grid.size(); i++) {
