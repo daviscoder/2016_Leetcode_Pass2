60ef5bc48a63e3c9ee6d337d52f08b2f221af592
daviscoder <yunfeng.chn@gmail.com>
2016-09-23 14:06:57 -0700

    Update 206. Reverse Linked List.cpp

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -1,42 +1,39 @@
 /**
- * 两种解法：recursion和iteration.
- * 
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ * O(n), O(1)
+ * 空list， 1个元素，2个，三个及以上
  */
- 
-// Recursion
-class Solution {
-public:
-    ListNode* helper (ListNode* head, ListNode* tail) {
-        if (head == NULL)
-            return tail;
-        ListNode * nextHead = head->next;
-        head->next = tail;
-        return helper (nextHead, head);
-    }
-    ListNode* reverseList(ListNode* head) {
-        return helper (head, NULL);
-    }
-};
- 
-// Iteration.
+
+// Iterative.
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         if (head == NULL || head->next == NULL)
             return head;
-        ListNode *p = head, *q = head->next;
+        ListNode * p = head, * q = head->next;
         p->next = NULL;
         while (q) {
-            ListNode *tmp = q->next;
+            ListNode * tmp = q->next;
             q->next = p;
             p = q;
             q = tmp;
+        }
+        return p;
+    }
+};
+
+// recursion
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL)
+            return head;
+        ListNode * p = head, * q = head->next;
+        p->next = NULL;
+        if (q) {
+            ListNode * newHead = reverseList (q);
+            q->next = p;
+            return newHead;
         }
         return p;
     }
