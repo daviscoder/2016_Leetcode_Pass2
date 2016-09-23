b462bbb24398e630beb7258e4d776d11b2d9ff44
daviscoder <yunfeng.chn@gmail.com>
2016-09-23 02:08:36 -0700

    Update 349. Intersection of Two Arrays.cpp

--- 349. Intersection of Two Arrays.cpp
+++ 349. Intersection of Two Arrays.cpp
@@ -1,19 +1,20 @@
-// 4 min 24s
+/**
+ * O(m + n), O(n)
+ * 空向量，有交集，无交集
+ */
 
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> mySet;
         vector<int> res;
+        unordered_set<int> nums;
         for (int i = 0; i < nums1.size(); i++) {
-            if (mySet.count (nums1[i]) == 0) {
-                mySet.insert (nums1[i]);
-            }
+            nums.insert (nums1[i]);
         }
         for (int i = 0; i < nums2.size(); i++) {
-            if (mySet.count (nums2[i])) {
+            if (nums.count(nums2[i])) {
                 res.push_back (nums2[i]);
-                mySet.erase (nums2[i]);
+                nums.erase (nums2[i]);
             }
         }
         return res;
